e3ee7fe78743a5e015de2dcf30d373a4866d36eb
JC.LV <lovex13@gmail.com>
2025-03-25 16:45:05 -0500

    v6

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ function Loguear() {
     }  
 
 }
+
+function register(){
+    let correo = document.getElementById('correo').value
+    let contraseña = document.getElementById ('contra').value
+    let confimarContraseña = document.getElementById('confi-contra')
+
+    if(contraseña == contraseña ){
+        alert('se a registrado con exito')
+    } else{
+        alert('las contraseñas no coincicen')
+    }
+}
