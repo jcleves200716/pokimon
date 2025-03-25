34581d87616784fb4b0274487c56a6a5bde1212b
JC.LV <lovex13@gmail.com>
2025-03-25 15:33:54 -0500

    v5

--- js/index.js
+++ js/index.js
@@ -3,11 +3,10 @@ function Loguear() {
     let username = document.getElementById('username').value
     let password = document.getElementById('clave').value
 
-    if(username == 'Rodolfo' && password == '123456'){
+    if(username == 'lovex14' && password == '123456'){
         alert('Usuario valido')
     }else{
         alert('usuario incorrecto')
     }  
 
 }
-
