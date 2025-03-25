a053d736ae6a4ba8981e8b3ea8438dad9e680bfd
JC.LV <lovex13@gmail.com>
2025-03-25 17:06:54 -0500

    v7

--- js/registro.js
+++ js/registro.js
@@ -1,16 +1,4 @@
 
-function Loguear() {
-    let username = document.getElementById('username').value
-    let password = document.getElementById('clave').value
-
-    if(username == 'lovex14' && password == '123456'){
-        alert('Usuario valido')
-    }else{
-        alert('usuario incorrecto')
-    }  
-
-}
-
 function register(){
     let correo = document.getElementById('correo').value
     let contraseña = document.getElementById ('contra').value
