b5d2293af4c0851a761237c127702f8a4f951568
JC.LV <lovex13@gmail.com>
2025-04-01 13:36:19 -0500

    v13

--- js/inicio.js
+++ js/inicio.js
@@ -10,9 +10,25 @@ function DatosHistoria() {
   
   // Crear y mostrar el mensaje de bienvenida
   const welcomeMessage = `
-      <p>Gracias por registrarte con nosotros ${usuario}, bienvenido a Pokimon, un lugar pensado</p>
-      <p>para que te salgas de lo cotidiano y tengas la oportunidad de
-      tener tus propios pokimons. Tu pokimon es ${nomPokimon} y tus propias batallas.</p>
+      <h3>¡Bienvenido al Mundo Pokimon, ${usuario}!</h3>
+        <p>Con ${edad} años, estás listo para comenzar tu aventura con tu Pokimon ${nomPokimon}.</p>
+        
+        <h4>La Leyenda de los Entrenadores Rivales</h4>
+        <p>En el mundo de Pokimon, hay dos entrenadores rivales, ${usuario}  y Ben, que han estado entrenando a sus pokimons durante años. Ambos buscan convertirse en el Campeón de Pokimon, pero solo uno puede lograrlo.</p>
+        
+        <h4>Los Pokimons Legendarios</h4>
+        <p>La leyenda habla de cinco pokimons legendarios:</p>
+        <ul>
+            <li><strong>Pikachu</strong>: Pokimon eléctrico que genera electricidad</li>
+            <li><strong>Charizard</strong>: Pokimon de fuego que vuela y genera llamas</li>
+            <li><strong>Blastoise</strong>: Pokimon de agua que genera olas y se protege con su caparazón</li>
+            <li><strong>Venusaur</strong>: Pokimon de hierba que genera veneno y controla plantas</li>
+            <li><strong>Dragonite</strong>: Pokimon dragón que vuela y genera vientos fuertes</li>
+        </ul>
+        
+        <p>Como nuevo entrenador, deberás elegir a tres de estos pokimons para formar tu equipo y competir. ¿Podrás derrotar a tus rivales y convertirte en el próximo Campeón Pokimon?</p>
+        
+        <p>Tu viaje comienza ahora con tu fiel ${nomPokimon}. ¡Buena suerte!</p>
   `;
   
   document.getElementById('welcomemessage').innerHTML = welcomeMessage;
