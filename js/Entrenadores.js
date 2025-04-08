c25e240ca7af90f6c6f218a69fbb06de2cf9b42a
JC.LV <lovex13@gmail.com>
2025-04-07 19:46:53 -0500

    v25

--- js/Entrenadores.js
+++ js/Entrenadores.js
@@ -7,18 +7,21 @@ function entrenadores() {
 
     if (!Nombre || !apellido || !telefono || !email ) {
         alert('Por favor llene los campos');
-        return;
+      
     }
 
    
 
-    localStorage.setItem('registroCorreo', correo);
-    localStorage.setItem('registroContra', contraseña);
-    localStorage.setItem('registroUsuario', usuario);
-    localStorage.setItem('registroEdad', edad)
+    localStorage.setItem('Nombre', Nombre);
+    localStorage.setItem('apellido', apellido);
+    localStorage.setItem('telefono', telefono);
+    localStorage.setItem('email', email)
+
     alert('registro exitoso')
   
-}   
+}  
+
+
 function pokemons() {
     const pokemon_nombre = document.getElementById('pokemon-nombre').value;
     const pokemon_tipo = document.getElementById('pokemon-tipo').value;
@@ -28,15 +31,17 @@ function pokemons() {
 
     if (!pokemon_nombre || !pokemon_tipo|| !pokemon_ataque|| !pokemon_defensa ) {
         alert('Por favor llene los campos');
-        return;
+      
+        
     }
 
     
 
-    localStorage.setItem('registroCorreo', correo);
-    localStorage.setItem('registroContra', contraseña);
-    localStorage.setItem('registroUsuario', usuario);
-    localStorage.setItem('registroEdad', edad)
+    localStorage.setItem('pokemon_nombre', pokemon-nombre);
+    localStorage.setItem('pokemon_tipo', pokemon-tipo);
+    localStorage.setItem('pokemon_ataque',pokemon-ataque);
+    localStorage.setItem('pokemon_defensa', pokemon-defensa)
 
     alert('Registro exitoso!');
+   
 }
